243f271ff4788e06cf642250dd0761ef419a9eca
ranggakrisna <infinity.duos@gmail.com>
2023-11-04 10:04:56 +0700

    add more code

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import {
 } from "./reducers/tictactoeReducer";
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { RepeatIcon } from "@chakra-ui/icons";
+import image1 from "./assets/gambar1.webp";
+import image2 from "./assets/gambar2.jpg";
 
 function Board() {
   const squares = useSelector((state) => state.states.squares);
@@ -32,6 +34,12 @@ function Board() {
   }
 
   function renderSquare(i) {
+    let textColor = "";
+    if (squares[i] === "X") {
+      textColor = "blue"; // Set the color for X
+    } else if (squares[i] === "O") {
+      textColor = "red"; // Set the color for O
+    }
     return (
       <Button
         width={"36"}
@@ -39,15 +47,15 @@ function Board() {
         bg={"transparent"}
         onClick={() => selectSquare(i)}
       >
-        <Text fontSize={"7xl"}>{squares[i]}</Text>
+        <Text fontSize={"7xl"} color={textColor}>
+          {squares[i]}
+        </Text>
       </Button>
     );
   }
 
-  const nextValue = calculateNextValue(squares);
   React.useEffect(() => {
     const winner = calculateWinner(squares);
-    console.log(winner);
     dispatch(setWinner(winner));
     if (winner === "X") {
       dispatch(setXWin());
@@ -58,8 +66,8 @@ function Board() {
     }
   }, [squares, dispatch]);
 
+  const nextValue = calculateNextValue(squares);
   const status = calculateStatus(winner, squares, nextValue);
-  console.log(status);
 
   return (
     <Box>
@@ -122,6 +130,102 @@ function Game() {
   const oWin = useSelector((state) => state.states.oWin);
   const draw = useSelector((state) => state.states.draw);
   const dispatch = useDispatch();
+  const playerXProfile = (
+    <Box
+      border={"4px"}
+      width={"200px"}
+      height={"200px"}
+      display={"flex"}
+      alignItems={"center"}
+      flexDir={"column"}
+      backgroundColor={"gray.700"}
+      borderColor={"teal.300"}
+      padding={"6"}
+      borderRadius={"30%"}
+    >
+      <Box
+        bgImage={`url(${image1})`}
+        bgSize={"cover"}
+        bgPosition={"center"}
+        bgRepeat={"no-repeat"}
+        width={"28"}
+        height={"28"}
+        marginTop={"-24"}
+        border={"4px"}
+        rounded={"full"}
+        backgroundColor={"teal.400"}
+      ></Box>
+      <Text
+        marginTop={"4"}
+        fontSize="2xl"
+        fontWeight={"semibold"}
+        color="white"
+      >
+        Red Bull
+      </Text>
+      <Box
+        marginTop={"3"}
+        bgColor={"red"}
+        width={"100px"}
+        padding={"1"}
+        rounded={"3xl"}
+        fontWeight={"extrabold"}
+        textAlign={"center"}
+        fontSize={"3xl"}
+        color={"yellow.100"}
+      >
+        X
+      </Box>
+    </Box>
+  );
+  const playerOProfile = (
+    <Box
+      border={"4px"}
+      width={"200px"}
+      height={"200px"}
+      display={"flex"}
+      alignItems={"center"}
+      flexDir={"column"}
+      backgroundColor={"gray.700"}
+      borderColor={"teal.300"}
+      padding={"6"}
+      borderRadius={"30%"}
+    >
+      <Box
+        bgImage={`url(${image2})`}
+        bgSize={"cover"}
+        bgPosition={"center"}
+        bgRepeat={"no-repeat"}
+        width={"28"}
+        height={"28"}
+        marginTop={"-24"}
+        border={"4px"}
+        rounded={"full"}
+        backgroundColor={"teal.400"}
+      ></Box>
+      <Text
+        marginTop={"4"}
+        fontWeight={"semibold"}
+        fontSize="2xl"
+        color="white"
+      >
+        Super Eagle
+      </Text>
+      <Box
+        marginTop={"3"}
+        bgColor={"red"}
+        width={"100px"}
+        padding={"1"}
+        rounded={"3xl"}
+        fontWeight={"extrabold"}
+        textAlign={"center"}
+        fontSize={"3xl"}
+        color={"yellow.100"}
+      >
+        O
+      </Box>
+    </Box>
+  );
   return (
     <Box bg={"tomato"}>
       <Text textAlign={"Center"} fontWeight={"semibold"} fontSize={"6xl"}>
@@ -130,85 +234,95 @@ function Game() {
       <Flex
         alignItems={"center"}
         justifyContent={"center"}
-        flexDir={"column"}
+        flexDir={"row"}
         marginX={"auto"}
         paddingBottom={"10"}
         color={"white"}
         width={"650px"}
         height={"auto"}
       >
+        <Box flex={"1"} marginRight={"40"}>
+          {playerXProfile}
+        </Box>
         <Box
+          border={"4px"}
           marginTop={"30"}
-          boxShadow={"lg"}
+          backgroundColor={"gray.700"}
+          borderColor={"teal.300"}
+          padding={"6"}
+          flex={"2"}
           rounded={"lg"}
-          padding={"10"}
-          bg={"orange.300"}
         >
-          <Flex
-            justifyContent={"center"}
-            alignItems={"center"}
-            marginBottom={"10"}
-            gap={"20px"}
-            fontSize={"2xl"}
-          >
-            <Box
-              display={"inline"}
-              padding={"4"}
-              rounded={"md"}
-              boxShadow={"lg"}
-              bgColor={"dimgray"}
+          <Box boxShadow={"lg"} rounded={"lg"} padding={"10"} bg={"orange.300"}>
+            <Flex
+              justifyContent={"center"}
+              alignItems={"center"}
+              marginBottom={"10"}
+              gap={"20px"}
+              fontSize={"2xl"}
             >
-              Win X : {xWin}
-            </Box>
-            <Box
-              display={"inline"}
-              padding={"4"}
-              rounded={"md"}
-              boxShadow={"lg"}
-              bgColor={"dimgray"}
+              <Box
+                display={"inline"}
+                padding={"4"}
+                rounded={"md"}
+                boxShadow={"lg"}
+                bgColor={"dimgray"}
+              >
+                Win X : {xWin}
+              </Box>
+              <Box
+                display={"inline"}
+                padding={"4"}
+                rounded={"md"}
+                boxShadow={"lg"}
+                bgColor={"dimgray"}
+              >
+                Draw : {draw}
+              </Box>
+              <Box
+                display={"inline"}
+                padding={"4"}
+                rounded={"md"}
+                boxShadow={"lg"}
+                bgColor={"dimgray"}
+              >
+                Win O : {oWin}
+              </Box>
+            </Flex>
+            <Board />
+            <Button
+              bgColor={"orange.400"}
+              _hover={{ bgColor: "orange.200" }}
+              width={"50"}
+              marginTop={"10"}
+              alignSelf={"flex-start"}
+              height={"16"}
+              onClick={() => dispatch(resetState())}
+              fontSize={"2xl"}
+              fontWeight={"semibold"}
+              boxShadow={"md"}
             >
-              Draw : {draw}
-            </Box>
-            <Box
-              display={"inline"}
-              padding={"4"}
-              rounded={"md"}
-              boxShadow={"lg"}
-              bgColor={"dimgray"}
+              <RepeatIcon marginRight={"2"} /> reset game
+            </Button>
+            <Button
+              bgColor={"orange.400"}
+              _hover={{ bgColor: "orange.200" }}
+              width={"50"}
+              marginLeft={"10"}
+              marginTop={"10"}
+              alignSelf={"flex-start"}
+              height={"16"}
+              onClick={() => dispatch(resetWin())}
+              fontSize={"2xl"}
+              fontWeight={"semibold"}
+              boxShadow={"md"}
             >
-              Win O : {oWin}
-            </Box>
-          </Flex>
-          <Board />
-          <Button
-            bgColor={"orange.400"}
-            _hover={{ bgColor: "orange.200" }}
-            width={"50"}
-            marginTop={"10"}
-            alignSelf={"flex-start"}
-            height={"16"}
-            onClick={() => dispatch(resetState())}
-            fontSize={"2xl"}
-            fontWeight={"semibold"}
-            boxShadow={"md"}
-          >
-            <RepeatIcon marginRight={"2"} /> reset game
-          </Button>
-          <Button
-            bgColor={"orange.400"}
-            _hover={{ bgColor: "orange.200" }}
-            width={"50"}
-            marginLeft={"20"}
-            marginTop={"10"}
-            alignSelf={"flex-start"}
-            height={"16"}
-            onClick={() => dispatch(resetWin())}
-            fontSize={"2xl"}
-            fontWeight={"semibold"}
-            boxShadow={"md"}
-          >
-            <RepeatIcon marginRight={"2"} /> reset winner
-          </Button>
+              <RepeatIcon marginRight={"2"} /> reset winner
+            </Button>
+          </Box>
+        </Box>
+        <Box flex={"1"} marginLeft={"40"}>
+          {playerOProfile}
         </Box>
       </Flex>
     </Box>
@@ -217,7 +331,7 @@ function Game() {
 
 // eslint-disable-next-line no-unused-vars
 function calculateStatus(winner, squares, nextValue) {
-  return winner === "X" && winner === "O"
+  return winner === "X" || winner === "O"
     ? `Winner: ${winner}`
     : winner === "draw"
     ? `Scratch: Cat's game`
